29b2c64169c57e60ac9bffd50bec6772fb9604c9
KayongoYongo <sknsammie10@gmail.com>
2023-02-07 21:09:35 +0000

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Definses the base model classs"""
+"""Defines the base model classs"""
 from uuid import uuid4
 from datetime import datetime
 
@@ -8,8 +8,34 @@ class BaseModel:
         """Represents the base model for the AirBnB project"""
 
         def __init__(self, *args, **kwargs):
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
+
+            """Initialize class"""
+            if kwargs:
+                for key, value in kwargs.items():
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    if key != '__class__':
+                        setattr(self, key, value)
+            else:
+                self.id = str(uuid4())
+                self.created_at = datetime.today()
+                self.updated_at = datetime.today()
+
+        def __str__(self):
+            """Return the print/str representation of the BaseModel instance."""
+            return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+        def save(self):
+            """Update updated_at with the current datetime."""
             self.updated_at = datetime.today()
 
-
+        def to_dict(self):
+            """Return the dictionary of the BaseModel instance.
+            Includes the key/value pair __class__ representing
+	        the class name of the object.
+            """
+            dict_representation = self.__dict__.copy()
+            dict_representation['__class__'] = self.__class__.__name__
+            dict_representation['created_at'] = self.created_at.isoformat()
+            dict_representation['updated_at'] = self.updated_at.isoformat()
+            return dict_representation
