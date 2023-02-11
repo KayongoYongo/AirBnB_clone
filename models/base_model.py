b266dc6ae55b9ff147819830d0e80904ea01b36c
KayongoYongo <sknsammie10@gmail.com>
2023-02-11 12:22:28 +0000

    base model pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -1,41 +1,44 @@
 #!/usr/bin/python3
 """Defines the base model classs"""
+
 from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel:
-        """Represents the base model for the AirBnB project"""
 
-        def __init__(self, *args, **kwargs):
+    """Represents the base model for the AirBnB project"""
+    def __init__(self, *args, **kwargs):
 
-            """Initialize class"""
-            if kwargs:
-                for key, value in kwargs.items():
-                    if key == 'created_at' or key == 'updated_at':
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    if key != '__class__':
-                        setattr(self, key, value)
-            else:
-                self.id = str(uuid4())
-                self.created_at = datetime.today()
-                self.updated_at = datetime.today()
-
-        def __str__(self):
-            """Return the print/str representation of the BaseModel instance."""
-            return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
-        def save(self):
-            """Update updated_at with the current datetime."""
+        """Initialize class"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
             self.updated_at = datetime.today()
+            models.storage.new(self)
 
-        def to_dict(self):
-            """Return the dictionary of the BaseModel instance.
-            Includes the key/value pair __class__ representing
-	        the class name of the object.
-            """
-            dict_representation = self.__dict__.copy()
-            dict_representation['__class__'] = self.__class__.__name__
-            dict_representation['created_at'] = self.created_at.isoformat()
-            dict_representation['updated_at'] = self.updated_at.isoformat()
-            return dict_representation
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
+
+    def save(self):
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+    def to_dict(self):
+        """Return the dictionary of the BaseModel instance.
+        """
+        dict_representation = self.__dict__.copy()
+        dict_representation['__class__'] = self.__class__.__name__
+        dict_representation['created_at'] = self.created_at.isoformat()
+        dict_representation['updated_at'] = self.updated_at.isoformat()
+        return dict_representation
