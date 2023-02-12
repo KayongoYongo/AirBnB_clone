8cc447457193eeabaf9092c691948c4963dbcd68
KayongoYongo <sknsammie10@gmail.com>
2023-02-12 20:45:07 +0000

    console update

--- console.py
+++ console.py
@@ -14,6 +14,94 @@ from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
+    class_dict = {
+             "BaseModel": BaseModel,
+             "User": User,
+             "State": State,
+             "City": City,
+             "Amenity": Amenity,
+             "Place": Place,
+             "Review": Review
+             }
+
+    def do_create(self, line):
+        """creates a class instance"""
+
+        if not line:
+            print("** class name missing **")
+        else:
+            if line not in HBNBCommand.class_dict:
+                print("** class doesn't exist **")
+            else:
+                my_model = HBNBCommand.class_dict[line]()
+                my_model.save()
+                print(my_model.id)
+
+    def do_show(self, line):
+        """prints string repr of an instance"""
+
+        key = my_obj(line)
+        if key:
+            my_dict = storage.all()
+            print(my_dict[key].to_dict())
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+
+        key = my_obj(line)
+        if key:
+            file_dict = storage.all()
+            del file_dict[key]
+            storage.save()
+
+    def do_all(self, line):
+        """prints string representation of objects"""
+
+        my_dict = storage.all()
+        my_list = []
+        if len(line) == 0:
+            for values in my_dict.values():
+                my_list.append(str(values))
+            print(my_list)
+        else:
+            if line not in HBNBCommand.class_dict:
+                print("** class doesn't exist **")
+            else:
+                for value in my_dict.values():
+                    if value.to_dict()["__class__"] == line:
+                        my_list.append(str(value))
+                    print(my_list)
+
+    def do_update(self, line):
+        """updates an instance"""
+
+        my_list = parse(line)
+        key = my_obj(line)
+        if key:
+            if len(my_list) > 4:
+                print("Usage:update <class name> <id>\
+                         <aittribute name> \"<attribute value>\"")
+            elif len(my_list) == 3:
+                print("** value missing **")
+            elif len(my_list) == 2:
+                print("** attribute name missing **")
+            else:
+                my_dict = storage.all()
+                my_in = my_dict[key]
+                val = my_list[3][1:-1]
+                try:
+                    if "." in val:
+                        val = float(val)
+                    else:
+                        val = int(val)
+                except ValueError:
+                    pass
+                setattr(my_in, my_list[2], val)
+                storage.save()
+
+    def parse(arg):
+        """Splits a line"""
+        return arg.split()
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
