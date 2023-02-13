05299f7b0e4f09e90e1bb3da89050f88fb27a567
KayongoYongo <sknsammie10@gmail.com>
2023-02-13 16:18:20 +0000

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines the file storage class"""
+"""Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -11,8 +11,7 @@ from models.review import Review
 
 
 class FileStorage:
-    """Represents an abstracted storage engine
-
+    """Represent an abstracted storage engine.
     Attributes:
         __file_path (str): The name of the file to save objects to.
         __objects (dict): A dictionary of instantiated objects.
